4c66f445987845f34ce35659fc5505afcb3fde58
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-10-17 14:34:08 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+  }
+
+  linux_profile {
+    admin_username = "aksadmin"
+    ssh_key {
+      key_data = file(var.ssh_public_key_path)
+    }
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = var.tags
+}
