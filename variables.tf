8e27a31ee3a5f1f7330b7906fcf827c681322398
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-10-21 11:27:27 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "resource_group_name" {
+  default = "rg-aks-demo"
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "aks_name" {
+  default = "aks-demo-cluster"
+}
+
+variable "dns_prefix" {
+  default = "aksdemo"
+}
+
+variable "node_count" {
+  default = 2
+}
+
+variable "node_vm_size" {
+  default = "Standard_D2s_v3"
+}
+
+variable "ssh_public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "tags" {
+  default = {
+    environment = "dev"
+    project     = "Project_11_AKS_Cluster_with_Terraform"
+  }
+}
